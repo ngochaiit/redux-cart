c39799394ad61bc6ce820e84b33a3664938cf0cd
Ngoc Hai <ngochaiit0701@gmail.com>
2018-12-27 18:34:18 +0700

    ready

---
+++ public/precache-manifest.e407562b69fcf70a938c0d79a11df926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4da578875579f40bf79a",
+    "url": "/static/js/main.4da57887.chunk.js"
+  },
+  {
+    "revision": "3bbc4cf72d0d61dc9068",
+    "url": "/static/js/1.3bbc4cf7.chunk.js"
+  },
+  {
+    "revision": "4da578875579f40bf79a",
+    "url": "/static/css/main.261949d5.chunk.css"
+  },
+  {
+    "revision": "90caeda860a93ed5d444c8d7fd378f54",
+    "url": "/index.html"
+  }
+];
